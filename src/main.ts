539a79a9d7f411a89fc00a41d077a41bd86956b9
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-01-17 17:51:56 -0400

    Adicionado type orm e arquitetura inicial

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import {AppModule} from './app.module';
 import Ssr = require('@react-ssr/nestjs-express/register');
 import {NestExpressApplication} from '@nestjs/platform-express';
 import {join} from 'path';
+import {ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     await Ssr(app);
+
+    app.useGlobalPipes(new ValidationPipe());
 
     app.useStaticAssets(join(__dirname, '..', 'public'));
 
