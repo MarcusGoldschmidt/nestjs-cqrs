be74744951d3c041b4e25123c2fe460ffd6fca7d
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-01-21 01:52:16 -0400

    Add roboto font

--- views/_document.tsx
+++ views/_document.tsx
@@ -13,6 +13,7 @@ export default class extends Document {
                 <title>React TOP TOP</title>
                 <meta charSet='utf-8'/>
                 <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width'/>
+                <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
                 <link rel='shortcut icon' href='/favicon.ico'/>
             </Head>
             <body>
